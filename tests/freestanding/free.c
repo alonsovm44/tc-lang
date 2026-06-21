b406521f5d995af25d07591717660bb0d3b0c043
Alonso <alvemo44@gmail.com>
2026-06-20 22:37:10 -0700

    freestanding demo

---
+++ tests/freestanding/free.c
@@ -0,0 +1,28 @@
+// Minimal type definitions for freestanding mode
+typedef __INT8_TYPE__ int8_t;
+typedef __INT16_TYPE__ int16_t;
+typedef __INT32_TYPE__ int32_t;
+typedef __INT64_TYPE__ int64_t;
+typedef __UINT8_TYPE__ uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
+typedef __INTPTR_TYPE__ intptr_t;
+typedef __UINTPTR_TYPE__ uintptr_t;
+typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#define NULL ((void*)0)
+#define volatile __volatile__
+
+#define TC_ALLOC(type, count) ((type *)calloc((count), sizeof(type)))
+#define TC_LENOF(x) (sizeof(x) / sizeof((x)[0]))
+#define TC_FAT_LENOF(x) ((x).len)
+
+
+int32_t main(void);
+
+
+int32_t main(void) {
+    
+    return 0;
+}
